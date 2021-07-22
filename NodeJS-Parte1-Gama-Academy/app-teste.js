166030208dc6c7d7cc7b6aa4fa66009deb4843be
Matt Rodrigues <ec.mattrodrigues@gmail.com>
2021-07-22 18:59:59 -0300

    Create perguntar ao browser
    aplicação que faz perguntas ao navegador direto da aba de endereço web

--- NodeJS-Parte1-Gama-Academy/app-teste.js
+++ NodeJS-Parte1-Gama-Academy/app-teste.js
@@ -1,14 +1,27 @@
+//inclui uma biblioteca
 const http = require('http');
+const url = require('url');
+const queryString = require('query-string');
 
-const HostName = '127.0.0.1';
-const Port = 3000;
+//Definição de endereço / URL
+const HostName = '127.0.0.1'; //LocalHost
+const Port = 3000; //Numero da porta
 
+// Implementação da regra de negócio
 const server = http.createServer((req, res) => {
+
+    //Pegar uma pergunta na URL
+    const params = queryString.parse(url.parse(req.url, true).search);
+    // console.log(params);
+    //Verificar a pergunta e escolher uma resposta
+    //Retornar a reposta escolhida
+
     res.statusCode = 200;
     res.setHeader('Context-Type', 'text/plain');
-    res.end('Hello World!');
+    res.end(params.pergunta);
 });
 
+//execução
 server.listen(Port, HostName, () => {
     console.log(`Server running at http://${HostName}:${Port}/`);
 });
